a8ea6e6464e5e28f4ac23f512b4a9065f7549d67
karsab <k.sablauskas@gmail.com>
2022-11-04 09:10:12 +0200

    initial-commit

---
+++ denovonet/training.py
@@ -0,0 +1,21 @@
+'''
+training.py
+
+Copyright (c) 2021 Karolis Sablauskas
+Copyright (c) 2021 Gelana Khazeeva
+
+This file is part of DeNovoCNN.
+
+DeNovoCNN is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DeNovoCNN is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+'''
